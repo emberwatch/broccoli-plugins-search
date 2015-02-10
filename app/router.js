c431e34075502952ef599e5247e67e9f2f9df777
Kerry Gallagher <k.gallagher.05@gmail.com>
2015-02-10 09:51:27 +0000

    Add in Google Analytics to the router

--- app/router.js
+++ app/router.js
@@ -5,6 +5,17 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+if (config.analyticsEnabled) {
+  Router.reopen({
+    sendGoogleAnalyticsEntry: function() {
+      return window.ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+    }.on('didTransition')
+  });
+}
+
 Router.map(function () {
   this.resource('packages', { path: '/' }, function () {
     this.route('list', { path: '/' });
